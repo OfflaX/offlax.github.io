f10f7fb5e59f2c45423a3c855ea93930cdff9906
OfflaX <offlaxis@gmail.com>
2023-03-21 09:58:51 +0200

    Updated blog

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def create_new_blog(title, content, cover_image):
     else:
         raise FileExistsError("File already exists, please check again your name! Aborting!")
 
-path_to_new_content = create_new_blog('Test_title', 'ghjk', 'logo.png')
+path_to_new_content = create_new_blog('Test_title', 'ghjk', 'content\\logo.png')
 
 # Index.html --> BLog post
 from bs4 import BeautifulSoup as Soup
