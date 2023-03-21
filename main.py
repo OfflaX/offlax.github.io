b17ca626e7d3d02cec1346045b601fc917dc15f7
OfflaX <offlaxis@gmail.com>
2023-03-21 09:58:29 +0200

    Updated blog

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import openai
 import git
 from pathlib import Path
+import shutil
 
 PATH_TO_BLOG_REPO = Path("C:\\Users\\atism\\Desktop\\GitHub_repositories\\offlax.github.io\\.git")
 PATH_TO_BLOG = PATH_TO_BLOG_REPO.parent
@@ -25,3 +26,42 @@ with open(PATH_TO_BLOG/'index.html', 'w') as file:
     file.write(random_text_string)
 
 update_blog()
+
+
+def create_new_blog(title, content, cover_image):
+    cover_image = Path(cover_image)
+
+    files = len(list(PATH_TO_CONTENT.glob("*html")))
+    new_title = f"{files+1}.html"
+    path_to_new_content = PATH_TO_CONTENT/new_title
+
+    shutil.copy(cover_image, PATH_TO_CONTENT)
+
+    if not os.path.exists(path_to_new_content):
+        # WRITE A NEW HTML FILE
+        with open(path_to_new_content, 'w') as file:
+            file.write('<!DOCTYPE HTML>\n')
+            file.write("<html>\n")
+            file.write("<head>\n")
+            file.write(f"<title> {title} </title>\n")
+            file.write("</head>\n")
+
+            file.write("<body>\n")
+            file.write(f"<img src='{cover_image} alt='Cover Image'><br>\n")
+            file.write(f"<h1>{title}<h1>")
+            # OpenAI --> Completion GPT --> "hello\nblog_post\n"
+            file.write(content.replace('\n', "<br />\n"))
+            file.write("</body>\n")
+            file.write("</html>\n")
+            print("Blog Created")
+            return path_to_new_content
+    else:
+        raise FileExistsError("File already exists, please check again your name! Aborting!")
+
+path_to_new_content = create_new_blog('Test_title', 'ghjk', 'logo.png')
+
+# Index.html --> BLog post
+from bs4 import BeautifulSoup as Soup
+with open(PATH_TO_BLOG/'index.html') as index:
+    soup = Soup(index.read())
+print(str(soup))
